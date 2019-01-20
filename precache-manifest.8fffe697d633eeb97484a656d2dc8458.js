29ef94ca8463b89b1a8e2f5f4710106a37723f18
Sergi <ocasergi@gmail.com>
2019-01-20 14:22:12 +0100

    Add files via upload

---
+++ precache-manifest.8fffe697d633eeb97484a656d2dc8458.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "dc1f5034617ec1771afd",
+    "url": "/static/js/main.dc1f5034.chunk.js"
+  },
+  {
+    "revision": "48856ba9482662b66556",
+    "url": "/static/js/1.48856ba9.chunk.js"
+  },
+  {
+    "revision": "dc1f5034617ec1771afd",
+    "url": "/static/css/main.58ece6eb.chunk.css"
+  },
+  {
+    "revision": "459cc030ae0e7c8a3fdf77353e5d5bd1",
+    "url": "/index.html"
+  }
+];
